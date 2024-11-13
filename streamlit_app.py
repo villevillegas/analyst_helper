d46797f3f09f3b7e95e1701026ec6f7688c9ae8f
Sergio Villegas <villevillegas2@gmail.com>
2024-11-12 22:25:03 -0600

    First impressions of streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+st.write("Hello Worldssss")
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
